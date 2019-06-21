36138789217d49a87cb9d42b67a328dd2dbd8272
Sam Markham <sam.e.markham@gmail.com>
2019-06-21 21:06:10 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,8 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
@@ -9,5 +11,6 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
 
 end
